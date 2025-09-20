886c299b3ea0918e57177a48ee99d3045de09306
Ryan <ryanpro0gamacaoedev@gmail.com>
2025-09-20 18:52:07 -0300

    ceboles

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string frase, ceboles;
+
+Console.Write("Escreva uma frase: ");
+frase = Console.ReadLine()!;
+ceboles =  frase
+.Replace("r", "l")
+.Replace("R", "L");
+Console.WriteLine(ceboles);
+
+
